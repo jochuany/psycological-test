18f862459f343b3f7f3acb00be367f634ced547c
JoChuan <ruochuanyang@gmail.com>
2025-05-01 15:48:10 +0800

    feat: add game state

--- src/app/croissant/page.js
+++ src/app/croissant/page.js
@@ -1,7 +1,6 @@
 "use client"
 
-import { DatePicker } from "antd";
-import CalenderPage from "@/app/component/object/CalenderPage";
+import { useState } from "react";
 
 import StartPage from "../component/page/StartPage";
 import QuestionPage from "../component/page/QuestionPage";
@@ -10,14 +9,46 @@ import ResultPage from "../component/page/ResultPage";
 
 export default function Croissant() {
 
+  const [gameState, setGameState] = useState({
+    state: 0, //測驗階段 0:start 1:question 2:displayResult 3:result
+    questionSate: 0,
+    totalQuestions: 3,
+    score: 0
+  });
+
+  const prevState = function () {
+    if (gameState.state <= 0) return null; // 小於零就結束這個function
+    console.log("prev")
+    setGameState({
+      ...gameState, //解構 gameState
+      state: gameState.state - 1
+    })
+  }
+
+  const nextState = function () {
+    if (gameState.state >= 3) return null; // 超過三就結束這個function
+    console.log("next")
+    setGameState({
+      ...gameState, //解構 gameState
+      state: gameState.state + 1
+    })
+  }
+
   return (
 
-    <div className="w-screen h-screen bg-gray-100 flex justify-center items-center">
+    <div className="w-screen h-screen bg-gray-100 flex  justify-center items-center">
 
-      <StartPage></StartPage>
-      <QuestionPage></QuestionPage>
-      <DisplayResultPage></DisplayResultPage>
-      <ResultPage></ResultPage>
+      {gameState.state == 0 && <StartPage></StartPage>}  {/* 前面 true 的話 回傳 && 後面 */}
+      {gameState.state == 1 && <QuestionPage></QuestionPage>}
+      {gameState.state == 2 && <DisplayResultPage></DisplayResultPage>}
+      {gameState.state == 3 && <ResultPage></ResultPage>}
+
+
+      <div onClick={prevState}>PREV</div>
+
+      <div onClick={nextState}>NEXT</div>
+
+
 
     </div>
   );
