e8d23306a26a24649d9f8893821c3367f2e5dd74
JoChuan <ruochuanyang@gmail.com>
2025-05-08 15:21:59 +0800

    start page layout

--- src/app/component/layout/MobileFrame.js
+++ src/app/component/layout/MobileFrame.js
@@ -3,7 +3,8 @@
 export default function MobileFrame({ children }) {
     return (
         <>
-            <div className="w-[314.4px] h-[682.4px] bg-white rounded-lg flex justify-center items-center">
+            <div className="w-[314.4px] h-[682.4px] bg-white rounded-lg flex justify-center items-center p-[36px]
+            relative overflow-hidden">
                 {children}
             </div>
         </>
--- src/app/component/page/StartPage.js
+++ src/app/component/page/StartPage.js
@@ -1,13 +1,40 @@
 "use client"
 
+import Image from "next/image";
 import MobileFrame from "../layout/MobileFrame";
 
-export default function StartPage() {
+import titleImg from "@/../public/0-start/title.svg";
+import startBtn from "@/../public/0-start/startBtn.png";
+import blurCircleY from "@/../public/0-start/blurCircleY.png";
+
+export default function StartPage({ nextStep }) {
     return (
         <>
             <MobileFrame>
-                start page
-            </MobileFrame>
+                {/* <div className="absolute -top-40 h-[314.4px] w-[314.4px] rounded-full bg-radial from-[#F2BE1260] from-[60%] to-[#F2BE1260] to-[0%]"></div> */}
+
+                <Image src={blurCircleY} alt="" className="absolute top-0 -translate-y-1/2"></Image>
+
+                <div className="flex flex-col justify-between items-center h-[46vh]">
+
+                    <Image src={titleImg} alt="你是哪一種可頌麵包？" className="w-[180px]"></Image>
+
+                    <div className="text-[12px] font-extrabold text-[#B65A0F] text-center leading-6">
+                        有些人天生酥脆，有些人出爐時就塌了。<br></br>
+                        你努力發酵、翻滾、等待出爐，<br></br>
+                        結果還是變成一坨可頌災難。<br></br>
+                        沒關係，這世界不缺完美麵包，<br></br>
+                        缺的是——像你一樣軟爛卻獨特的存在。<br></br>
+                        現在，就來看看你是什麼等級的失控可頌吧。
+                    </div>
+
+                    <Image src={startBtn} alt="START" onClick={nextStep} className="w-[100px]"></Image>
+
+                </div>
+
+                <Image src={blurCircleY} alt="" className="absolute bottom-0 translate-y-1/2"></Image>
+
+            </MobileFrame >
         </>
     );
 }
