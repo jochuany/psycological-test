baef799085f09a6bc84a6f15b68034588d659699
JoChuan <ruochuanyang@gmail.com>
2025-05-15 14:25:17 +0800

    add question store function

--- src/app/store/store.js
+++ src/app/store/store.js
@@ -14,5 +14,35 @@ const usePsyStore = create((set) => ({
     updateScore: (newState) => set((state) => ({ score: newState }))
 }))
 
+// 問題管理
+const useQuestionStore = create((set) => ({
+    question: {
+        1: {
+            title: "麵包師傅要你「靜置 30 分鐘」，你會怎麼做？",
+            options: [
+                { title: "乖乖待著⋯ 然後偷偷膨脹三倍大", value: 5 },
+                { title: "等個屁！我已經開始發酵狂飆了", value: 5 },
+                { title: "發⋯ 什麼？我忘記了，我睡著了", value: 5 },
+            ]
+        },
+        2: {
+            title: "當你被放進烤箱時，溫度突然升高，你的反應是？",
+            options: [
+                { title: "啊啊啊啊啊啊！（冒泡炸裂）", value: 5 },
+                { title: "熱熱熱快翻面！我要烤出最酥脆的皮！", value: 5 },
+                { title: "我已經放棄掙扎，來吧命運⋯⋯", value: 5 },
+            ]
+        },
+        3: {
+            title: "如果你被顧客挑選時被放回去了，你會？",
+            options: [
+                { title: "立刻乾癟五公分，氣到扁掉", value: 5 },
+                { title: "更用力散發麵包香，讓他後悔！", value: 5 },
+                { title: "裝死，假裝自己是牛角麵包", value: 5 },
+            ]
+        }
+    },
+}))
+
 
 export { usePsyStore }
